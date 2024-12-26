45996a952929e8556dd669b19e625bfd9c7b2854
luisdanielta <luisdaniel.ta@outlook.com>
2024-12-25 23:26:01 -0500

    feat: Update page layout and components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	import NavBar from '$lib/components/navBar.svelte';
 	import NavFloat from '$lib/components/navFloat.svelte';
-	import Tabs from '$lib/components/section/tabs.svelte';
 </script>
 
 <NavBar />
