bf071bc792d8ee7ad41e9342e31a72ad1f5fa07c
luisdanielta <luisdaniel.ta@outlook.com>
2024-12-29 13:19:59 -0500

    chore: Remove unused Dockerfile for server and pb directories, and locals types for pb

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,11 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			pb: PocketBase;
+			user: Admin | Record | null;
+			security: Security;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
