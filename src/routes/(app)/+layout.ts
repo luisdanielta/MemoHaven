84ed41ddfed87a956f7abbf76b1bdbc45a1637f8
luisdanielta <luisdaniel.ta@outlook.com>
2024-12-30 06:27:35 -0500

    refactor: Update protectedRoute.svelte and +layout.ts
    - In protectedRoute.svelte, change the condition in the if statement to isAuthenticated()
    - In +layout.ts, uncomment the line that redirects to '/sign/in' if the user is not authenticated

--- src/routes/(app)/+layout.ts
+++ src/routes/(app)/+layout.ts
@@ -2,5 +2,5 @@ import { isAuthenticated } from '$lib/context/user.svelte';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
-	// if (!isAuthenticated()) return redirect(307, '/sign/in');
+	if (!isAuthenticated()) return redirect(307, '/sign/in');
 };
